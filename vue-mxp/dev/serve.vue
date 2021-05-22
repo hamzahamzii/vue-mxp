916fd1649d56a22c436769d0d978de697532adf8
hamzahamzii <hmza.ehmad@gmail.com>
2021-05-22 11:51:37 +0500

    new classes code

--- vue-mxp/dev/serve.vue
+++ vue-mxp/dev/serve.vue
@@ -2,13 +2,11 @@
 import { defineComponent } from "vue";
 // Uncomment import and local "components" registration if library is not registered globally.
 import { mxpButton } from "@/entry.esm";
-import { mxpInput } from "@/entry.esm";
 
 export default defineComponent({
   name: "ServeDev",
   components: {
     mxpButton,
-    mxpInput,
   },
 });
 </script>
@@ -16,6 +14,5 @@ export default defineComponent({
 <template>
   <div class="flex" id="app">
     <mxpButton rounded square>YOOO</mxpButton>
-    <mxpInput />
   </div>
 </template>
