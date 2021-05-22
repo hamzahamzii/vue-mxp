5cb4bc63fd40b0b389389525f22fde2ef331a347
hamzahamzii <hmza.ehmad@gmail.com>
2021-05-22 11:20:17 +0500

    props added

--- vue-mxp/src/lib-components/mxpButton.vue
+++ vue-mxp/src/lib-components/mxpButton.vue
@@ -8,5 +8,17 @@
 import { defineComponent } from "vue";
 export default defineComponent({
   name: "mxpButton",
+  props: {
+    rounded: Boolean,
+    square: Boolean,
+    secondary: Boolean,
+    subtle: Boolean,
+    primary: Boolean,
+
+    filled: Boolean,
+    stroke: Boolean,
+    small: Boolean,
+    text: Boolean,
+  },
 });
 </script>
