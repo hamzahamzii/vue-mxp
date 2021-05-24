1a7ac6ee191e251cebfa02288d406062207b7df0
hamzahamzii <hmza.ehmad@gmail.com>
2021-05-24 20:36:13 +0500

    Publish 1.1.1

--- vue-mxp/dist/vue-mxp.esm.js
+++ vue-mxp/dist/vue-mxp.esm.js
@@ -1,4 +1,4 @@
-import { defineComponent, openBlock, createBlock, createVNode, renderSlot } from 'vue';
+import { defineComponent, openBlock, createBlock, createVNode, renderSlot, createCommentVNode } from 'vue';
 
 var script$1 = defineComponent({
   name: "mxpButton",
@@ -17,18 +17,21 @@ var script$1 = defineComponent({
     primary: Boolean,
     filled: Boolean,
     stroke: Boolean,
-    text: Boolean
+    text: Boolean,
+    // Icons
+    loader: Boolean
   },
   computed: {
     computedClasses() {
       return {
         "px-5 py-1": !this.small,
         "px-3 py-1 text-xs": this.small,
-        "bg-white text-blue hover:text-blue-dark rounded-lg": this.text && !this.filled && !this.stroke,
+        "bg-white text-blue hover:text-blue-dark rounded-lg focus:outline-none focus:bg-blue-lightest": this.text && !this.filled && !this.stroke,
         "rounded-lg": this.rounded && !this.square && !this.text,
         "rounded-none": this.square && !this.rounded && !this.text,
         "border-2 border-blue hover:border-blue-dark hover:text-blue-dark bg-white text-blue": this.stroke && !this.filled,
-        "bg-blue hover:bg-blue-dark text-white": this.filled && !this.stroke
+        "bg-blue hover:bg-blue-dark  text-white": this.filled && !this.stroke,
+        "focus:outline-none focus:ring-4 text-white active:bg-blue-dim active:ring active:ring-blue active:ring-8 active:ring-offset-0 active:rounded-full": !this.text
       };
     },
 
@@ -41,11 +44,28 @@ var script$1 = defineComponent({
   }
 });
 
+const _hoisted_1$1 = /*#__PURE__*/createVNode("path", {
+  d: "M25,5A20.14,20.14,0,0,1,45,22.88a2.51,2.51,0,0,0,2.49,2.26h0A2.52,2.52,0,0,0,50,22.33a25.14,25.14,0,0,0-50,0,2.52,2.52,0,0,0,2.5,2.81h0A2.51,2.51,0,0,0,5,22.88,20.14,20.14,0,0,1,25,5Z"
+}, null, -1);
+
+const _hoisted_2 = {
+  key: 2,
+  class: "text-blue"
+};
 function render$1(_ctx, _cache, $props, $setup, $data, $options) {
   return openBlock(), createBlock("div", null, [createVNode("button", {
     style: _ctx.computedStyles,
-    class: [_ctx.computedClasses, "active:bg-red m-2"]
-  }, [renderSlot(_ctx.$slots, "default")], 6)]);
+    class: [_ctx.computedClasses, "active:bg-blue-dim active:ring active:ring-blue active:ring-4 active:ring-offset-0 m-2"]
+  }, [!_ctx.loader ? renderSlot(_ctx.$slots, "default", {
+    key: 0
+  }) : createCommentVNode("", true), _ctx.loader && !_ctx.text ? (openBlock(), createBlock("svg", {
+    key: 1,
+    fill: [_ctx.secondary || _ctx.stroke ? 'skyblue' : 'white'],
+    class: "animate-spin",
+    width: "15",
+    height: "15",
+    viewBox: "0 0 50 50"
+  }, [_hoisted_1$1], 8, ["fill"])) : createCommentVNode("", true), _ctx.loader && _ctx.text && !_ctx.filled && !_ctx.subtle && !_ctx.secondary ? (openBlock(), createBlock("span", _hoisted_2, "loading...")) : createCommentVNode("", true)], 6)]);
 }
 
 script$1.render = render$1;
