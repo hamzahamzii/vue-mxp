19f2919badd43762543c16fd7b9aae4ecf120b03
hamzahamzii <hmza.ehmad@gmail.com>
2021-05-25 20:09:19 +0500

    text field size increased

--- vue-mxp/dev/serve.vue
+++ vue-mxp/dev/serve.vue
@@ -14,15 +14,30 @@ export default defineComponent({
 <template>
   <div class="flex" id="app">
     <mxpButton disabled rounded filled>Button</mxpButton>
-    <mxpButton  rounded filled>Button</mxpButton>
+    <mxpButton square filled>Button</mxpButton>
+    <mxpButton stroke rounded>Button</mxpButton>
+    <mxpButton rounded filled>Button</mxpButton>
     <mxpButton subtle rounded loader>Button</mxpButton>
-    <mxpButton subtle rounded >Button</mxpButton>
+    <mxpButton subtle rounded>Button</mxpButton>
 
-
-    <mxpTextInput placeholder="This is a placeholder" ></mxpTextInput>
-    <mxpTextInput disabled placeholder="This is a disabled input" ></mxpTextInput>
-    <mxpTextInput successMessage="This is a success message" placeholder="I am green" ></mxpTextInput>
-    <mxpTextInput description small placeholder="This is a placeholder" ></mxpTextInput>
-    <mxpTextInput description errorMessage="This is an error message" placeholder="This is a placeholder" ></mxpTextInput>
+    <mxpTextInput placeholder="This is a placeholder"></mxpTextInput>
+    <mxpTextInput
+      disabled
+      placeholder="This is a disabled input"
+    ></mxpTextInput>
+    <mxpTextInput
+      successMessage="This is a success message"
+      placeholder="I am green"
+    ></mxpTextInput>
+    <mxpTextInput
+      description
+      small
+      placeholder="This is a placeholder"
+    ></mxpTextInput>
+    <mxpTextInput
+      description
+      errorMessage="This is an error message"
+      placeholder="This is a placeholder"
+    ></mxpTextInput>
   </div>
 </template>
--- vue-mxp/src/lib-components/mxpTextInput.vue
+++ vue-mxp/src/lib-components/mxpTextInput.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="flex flex-col">
-    <input v-if="!description" :placeholder="placeholder" :disabled="disabled" :width="width" type="text" 
-     class="outline-none bg-gray-100 rounded-lg text-xs p-2 mx-2 mt-2 active:border-2 border-black focus:border-2 border-black"
-    :style="computedStyles"
-    :class="computedClasses"
-    >
-    <textarea v-else :placeholder="placeholder" :disabled="disabled"
-     class="outline-none rounded-lg bg-gray-100 text-xs mx-2 mt-2 active:border-2 border-black focus:border-2 border-black"
-     style="padding:12px!important"
-     :style="computedStyles"
-     :class="computedClasses"
+    <input
+      v-if="!description"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :width="width"
+      type="text"
+      class="outline-none bg-gray-100 rounded-lg p-2 mx-2 mt-2 active:border-2 border-black focus:border-2 border-black"
+      :style="computedStyles"
+      :class="computedClasses"
+    />
+    <textarea
+      v-else
+      :placeholder="placeholder"
+      :disabled="disabled"
+      class="outline-none rounded-lg bg-gray-100 mx-2 mt-2 active:border-2 border-black focus:border-2 border-black"
+      style="padding: 12px !important"
+      :style="computedStyles"
+      :class="computedClasses"
     ></textarea>
-    <small v-if="caption" class="text-gray-500 mx-2">{{caption}}</small>
-    <small v-if="successMessage" class="text-green-500 mx-2">{{successMessage}}</small>
-    <small v-if="errorMessage" class="text-red-500 mx-2">{{errorMessage}}</small>
+    <small v-if="caption" class="text-gray-500 mx-2">{{ caption }}</small>
+    <small v-if="successMessage" class="text-green-500 mx-2">{{
+      successMessage
+    }}</small>
+    <small v-if="errorMessage" class="text-red-500 mx-2">{{
+      errorMessage
+    }}</small>
   </div>
 </template>
 
@@ -51,12 +63,10 @@ export default defineComponent({
     computedClasses(): any {
       return {
         // small inputs
-        "px-1 py-1":this.small,
-
-        "bg-green-100 border-2 border-green-500":this.successMessage,
-        "bg-red-100 border-2 border-red-500":this.errorMessage,
-        "bg-gray-100 border-none":this.disabled,
-        
+        "px-1 py-1": this.small,
+        "bg-green-100 border-2 border-green-500": this.successMessage,
+        "bg-red-100 border-2 border-red-500": this.errorMessage,
+        "bg-gray-100 border-none": this.disabled,
       };
     },
 
