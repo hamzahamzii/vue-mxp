005b0c1fb4b43a9d0d538ecc0630f9a2d5ec0ebf
hamzahmzii <hmza.ehmad@gmail.com>
2021-06-01 09:13:46 +0500

    form

--- vue-mxp/dev/componentsDisplay/testForm.vue
+++ vue-mxp/dev/componentsDisplay/testForm.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="flex w-1/2 flex-col px-2 items-center justify-center">
+  <div class="flex w-1/2 flex-col px-2">
     <h1 class="display-medium-bold m-4">Signup</h1>
-    <mxpTextInput username small placeholder="User name" />
+    <mxpTextInput class="mb-2" username small placeholder="User name" />
+    <mxpTextInput username small placeholder="Password" />
+    <mxpButton class="float-right" rounded filled>Signup</mxpButton>
   </div>
 </template>
