035278ccdc479be1a4e7a9273b0f8c62575e0644
hamzahmzii <hmza.ehmad@gmail.com>
2021-06-01 22:26:39 +0500

    form width changed

--- vue-mxp/dev/componentsDisplay/testForm.vue
+++ vue-mxp/dev/componentsDisplay/testForm.vue
@@ -1,19 +1,30 @@
 <template>
-<h1 class="display-medium-bold m-4">Signup</h1>
-  <div class="p-10 m-3 shadow-2xl rounded-lg">
-    <mxpTextInput class="my-4 shadow-md" username small placeholder="User name" />
+  <h1 class="display-medium-bold m-4">Signup</h1>
+  <div class="p-10 m-3 w-full max-w-xl shadow-2xl">
+    <mxpTextInput
+      class="my-4 shadow-md"
+      username
+      small
+      placeholder="User name"
+    />
     <mxpTextInput class="my-4 shadow-md" email small placeholder="Email" />
-    <mxpTextInput type="Password" class="my-4 shadow-md" pass small placeholder="Password" />
+    <mxpTextInput
+      type="Password"
+      class="my-4 shadow-md"
+      pass
+      small
+      placeholder="Password"
+    />
     <div class="flex my-4">
-    <mxpRadioButton ></mxpRadioButton>
-    <small class="my-1 mx-1">Male</small>
-    <mxpRadioButton class="mx-1"></mxpRadioButton>
-    <small class="my-1 mx-1">Female</small>
+      <mxpRadioButton></mxpRadioButton>
+      <small class="my-1 mx-1">Male</small>
+      <mxpRadioButton class="mx-1"></mxpRadioButton>
+      <small class="my-1 mx-1">Female</small>
     </div>
     <div class="flex my-4">
       <mxpCheckBox></mxpCheckBox>
       <small class="my-1 mx-1">I accept all the terms and conditions.</small>
     </div>
-     <mxpButton class="sm:float-right my-3" rounded filled>Signup</mxpButton>
+    <mxpButton class="sm:float-right my-3" rounded filled>Signup</mxpButton>
   </div>
 </template>
